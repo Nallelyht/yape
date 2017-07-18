3451f9615360de73eb3e60d92b07cd725177ce2c
Nallely House <nallelyht@gmail.com>
2017-07-18 16:39:01 -0500

    registro

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+var cargarPagina = function () {
 	$('.carousel.carousel-slider').carousel({fullWidth: true});
-});
+	
+	validarNumero();
+	
+};
+var validarNumero = function() {
+	var $boton = $('#btn-continuar');
+	var $terminos = $('#terminos');
+	var $registroTelefono = $('#numero-registro');
+	
+};
+$(document).ready(cargarPagina);
